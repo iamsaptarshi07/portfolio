b391b8f2a5e35627794ef06cccf0bf9c83125494
Saptarshi <iamsaptarshi07@gmail.com>
2024-06-24 19:39:26 +0530

    Added LSTM project in project.py

--- projects.py
+++ projects.py
@@ -1,3 +1,4 @@
 PROJECTS = {
     "🏆 DIGITAL RESUME": "https://github.com/iamsaptarshi07/portfolio",
+    "🏆 LSTM-BASED MODEL TO PREDICT STOCK PRICE": "https://github.com/iamsaptarshi07/LSTM-Based-Model-to-Predict-Stock-Price",
 }
