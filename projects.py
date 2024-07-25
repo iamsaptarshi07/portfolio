e0dbb0a08aed303e16543dd353aa02b95b3c4ee2
Saptarshi <iamsaptarshi07@gmail.com>
2024-07-25 22:59:55 +0530

    Modified projects.py

--- projects.py
+++ projects.py
@@ -1,4 +1,5 @@
 PROJECTS = {
     "🏆 DIGITAL RESUME": "https://github.com/iamsaptarshi07/portfolio",
     "🏆 LSTM-BASED MODEL TO PREDICT STOCK PRICE": "https://github.com/iamsaptarshi07/LSTM-Based-Model-to-Predict-Stock-Price",
+    "🏆 PERSONALIZED RECOMMENDER SYSTEM USING NEURAL COLLABORATIVE FILTERING": "https://github.com/iamsaptarshi07/Personalized-Recommender-System",
 }
